27163048e6b6bbce7409ed68cf12825caf959b0d
Jon Gauthier <jon@gauthiers.net>
2018-10-24 18:00:15 -0400

    parsing in projected AG half-working

--- infinite_parser/inference/adaptor.py
+++ infinite_parser/inference/adaptor.py
@@ -4,10 +4,11 @@ Pitman-Yor adaptor grammar inference by MCMC.
 
 from collections import defaultdict, Counter
 from copy import copy
+import itertools
 
 import numpy as np
 
-from infinite_parser.inference.inside_outside import parse
+from infinite_parser.inference.inside_outside import InsideOutsideParser
 from infinite_parser.pcfg import FixedPCFG
 
 
@@ -15,12 +16,80 @@ def _index(xs):
   return {x: idx for idx, x in enumerate(xs)}
 
 
-def parse_io(projected_pcfg, sentence):
+ADAPTED_PRETERMINAL = "#AD"
+
+
+class InsideOutsideProjectedParser(InsideOutsideParser):
   """
-  Parse a sentence using the PCFG projected from an adaptor grammar.
+  Inside-outside parser modified to work with PCFGs that are projections of an
+  adaptor grammar.
+
+  Projected PCFGs have a special logic for assigning probabilities to adapted
+  rewrites.
   """
-  # TODO
-  pass
+
+  def __init__(self, pcfg):
+    self.pcfg = pcfg
+
+  def _inside(self, sentence):
+    pcfg = self.pcfg
+
+    alpha = np.zeros((len(pcfg.nonterminals) + len(pcfg.preterminals),
+                      len(sentence), len(sentence)))
+    backtrace = np.zeros((len(pcfg.nonterminals) + len(pcfg.preterminals),
+                          len(sentence), len(sentence), 3), dtype=int)
+    # base case: unary rewrites
+    for i, preterm in enumerate(pcfg.preterminals):
+      for j, word in enumerate(sentence):
+        weight = pcfg.unary_weights[i, pcfg.term2idx[word]]
+
+        # preterminals are indexed after nonterminals in alpha array.
+        idx = len(pcfg.nonterminals) + i
+        alpha[idx, j, j] = weight
+
+    # recursive case
+    for span in range(2, len(sentence) + 1):
+      for j in range(0, len(sentence) - span + 1):
+        # End of nonterminal span (up to and including the word at this index)
+        k = j + span - 1
+        # where `end` denotes an end up to and including the word at index `end`
+        for i, nonterm in enumerate(pcfg.nonterminals):
+          score = 0
+
+          # Keep backtrace for maximally scoring element
+          best_backtrace, best_backtrace_score = None, 0
+
+          for split in range(1, span):
+            for prod_idx, (left, right) in enumerate(pcfg.productions):
+              # Prepare index lookups for left/right children.
+              left_idx = len(pcfg.nonterminals) + pcfg.preterm2idx[left] \
+                  if left in pcfg.preterm2idx else pcfg.nonterm2idx[left]
+              right_idx = len(pcfg.nonterminals) + pcfg.preterm2idx[right] \
+                  if right in pcfg.preterm2idx else pcfg.nonterm2idx[right]
+
+              # Calculate inside probabilities of left and right children.
+              left_score = alpha[left_idx, j, j + split - 1]
+              right_score = alpha[right_idx, j + split, k]
+
+              local_score = np.exp(
+                  # Production score
+                  np.log(pcfg.binary_weights[pcfg.nonterm2idx[nonterm], prod_idx]) +
+                  # Left child score
+                  np.log(left_score) +
+                  # Right child score
+                  np.log(right_score))
+
+              score += local_score
+
+              if local_score > best_backtrace_score:
+                best_backtrace = (left_idx, right_idx, split)
+                best_backtrace_score = local_score
+
+          alpha[i, j, k] = score
+          if best_backtrace is not None:
+            backtrace[i, j, k] = best_backtrace
+
+    return alpha, backtrace
 
 
 class AdaptorGrammar(object):
@@ -80,45 +149,52 @@ class AdaptorGrammar(object):
     # Because parsing assumse CNF, we first have to prepare to produc
     # CNF-friendly rewrites of adapted nonterminals to their yields.
 
+    # Create a special preterminal for adapted subtrees.
+    preterminals = copy(self.preterminals)
+    assert ADAPTED_PRETERMINAL not in preterminals
+    preterminals.append(ADAPTED_PRETERMINAL)
+
     # An adapted nonterminal rewritten to `k` children will require `k - 2`
     # subsymbols in the CNF. We'll use this character to denote those
     # subsymbols.
     cnf_rewrite_char = "#"
 
+    words_in_adapted_yields = defaultdict(set)
     nonterminals = copy(self.nonterminals)
+    productions = copy(self.productions)
     nonterminal_adapted_weights = {}
-    for adapted_nonterm, productions in self.adapted_productions.items():
+    for adapted_nonterm, adapted_productions in self.adapted_productions.items():
       max_production_length = 0
-      production_weights = {}
+      adapted_production_weights = {}
 
-      for production in productions:
-        max_production_length = max(max_production_length, len(production))
+      for adapted_production in adapted_productions:
+        max_production_length = max(max_production_length, len(adapted_production))
 
         # TODO compute grammar weight for this adapted rewrite.
         weight = 0.0
-        production_weights[production] = weight
+        adapted_production_weights[adapted_production] = weight
 
-      nonterminal_adapted_weights[adapted_nonterm] = production_weights
+        words_in_adapted_yields[adapted_nonterm] |= set(adapted_production)
+
+      nonterminal_adapted_weights[adapted_nonterm] = adapted_production_weights
 
       # Add the relevant number of CNF-friendly nonterminals.
-      nonterminals.extend(["%s%s" % (adapted_nonterm, cnf_rewrite_char * i)
-                           for i in range(0, max_production_length - 1)])
-
-    # Create a special preterminal for adapted subtrees.
-    adapted_preterm = "#AD"
-    preterminals = copy(self.preterminals)
-    assert adapted_preterm not in preterminals
-    preterminals.append(adapted_preterm)
+      new_nonterms = ["%s%s" % (adapted_nonterm, cnf_rewrite_char * i)
+                      for i in range(0, max_production_length - 1)]
+      nonterminals.extend(new_nonterms)
+      # Also add the relevant production rules.
+      productions.extend([(ADAPTED_PRETERMINAL, new_nonterm)
+                          for new_nonterm in new_nonterms])
 
     # TODO prep weights
-    # TODO update productions
 
     return ProjectedPCFG(
         start=self.start,
         terminals=self.terminals,
         nonterminals=nonterminals,
         preterminals=preterminals,
-        productions=self.productions)
+        productions=productions,
+        words_in_adapted_yields=words_in_adapted_yields)
 
     # # Production list: all grammar production rules + yields of adapted
     # # nonterminals
@@ -166,5 +242,6 @@ class AdaptorGrammar(object):
 
 class ProjectedPCFG(FixedPCFG):
 
-  def __init__(self, *args, **kwargs):
+  def __init__(self, words_in_adapted_yields=None, *args, **kwargs):
     super().__init__(*args, **kwargs)
+    self.words_in_adapted_yields = words_in_adapted_yields
--- test/test_adaptor.py
+++ test/test_adaptor.py
@@ -5,27 +5,28 @@ import numpy as np
 from scipy.special import digamma
 
 from infinite_parser.inference.adaptor import *
-from infinite_parser.inference.inside_outside import parse
 from infinite_parser.pcfg import FixedPCFG, tree_from_backtrace
 
 
 
 def _dummy_ag():
-  return AdaptorGrammar(
+  ag = AdaptorGrammar(
       start='Word',
       terminals=['a', 'b', 'c'],
       nonterminals=['Word', 'Morph', 'Morph$'],
       preterminals=['Phoneme'],
       adapted_nonterminals=['Morph'],
-      productions=[('Morph', 'Morph$')])
+      productions=[('Morph', 'Morph'), ('Phoneme', 'Phoneme')])
+
+  # Simulate an adapted production "aba"
+  # ag.adapted_productions['Morph'][Tree.fromstring("(Morph (Phoneme a) (Morph$ (Phoneme b) (Phoneme a)))")] = 1.0
+  ag.adapted_productions['Morph'][("a", "b", "a", "b")] = 1.0
+
+  return ag
 
 
 def test_project_pcfg():
   ag = _dummy_ag()
-
-  # Simulate an adapted production "aba"
-  # ag.adapted_productions['Morph'][Tree.fromstring("(Morph (Phoneme a) (Morph$ (Phoneme b) (Phoneme a)))")] = 1.0
-  ag.adapted_productions['Morph'][("a", "b", "a")] = 1.0
   pcfg = ag.project_pcfg()
 
   adapted_preterm = "#AD"
@@ -36,3 +37,35 @@ def test_project_pcfg():
       "CNF-friendly binarized nonterminals of adapted nonterminal 'Morph' should exist"
 
   # TODO test weights
+
+
+def test_parse_projected_pcfg():
+  ag = _dummy_ag()
+  pcfg = ag.project_pcfg()
+  parser = InsideOutsideProjectedParser(pcfg)
+
+  sentence = "a b a b".split()
+  alphas, betas, backtrace = parser.parse(sentence)
+
+  t = tree_from_backtrace(pcfg, sentence, backtrace)
+  t.pretty_print()
+  assert t is not None
+  # TODO check that adapted nonterminal is an option
+  non_adapted_parse = Tree.fromstring("(Word (Morph (Phoneme a) (Phoneme b)) (Morph (Phoneme a) (Phoneme b)))")
+  # TODO check that non-adapted parse is also an option
+
+  return
+
+  # Test that we can parse other sentences, too.
+  sentence = "a b".split()
+  alphas, betas, backtrace = parser.parse(sentence)
+  t = tree_from_backtrace(pcfg, sentence, backtrace)
+  # TODO
+
+  # Test that we can parse a sentence containing the adapted rewrite
+  sentence = "a b a b".split()
+  alphas, betas, backtrace = parser.parse(sentence)
+  t = tree_from_backtrace(pcfg, sentence, backtrace)
+  # TODO check that adapted nonterminal is an option
+  # TODO check that non-adapted parse is also an option
+
